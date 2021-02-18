2f32696142defa03852d3ad31abad9d7f0813caa
SetManMr <nurlan.karabukaev@gmail.com>
2021-02-19 05:41:08 +0600

    other

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,43 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-xxl">
-              <a class="navbar-brand" href="#">Food.com</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
+@extends('layouts.layout', ['site_name' => 'Main'])
 
-                <form class="d-flex">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-              </div>
-            </div>
-          </nav>
-
-    </header>
+@section('content')
+  @if(isset($_GET['search']))
+    @if(count($posts)>0)
+        <h2>Результаты по запросу <?=$_GET['search']?></h2>
+        <p class="lead">Всего найдено {{ count($posts) }} данных</p>
+    @else
+        <h2>По запросу <?=$_GET['search']?> Ничего не найдено</h2>
+        <a href="{{ route('post.index') }}" class="btn btn-outline-primary">Отоброзить всё</a>
+    @endif
+  @endif
 
     <section>
         <div class="container-xxl">
@@ -240,7 +213,6 @@
     </section>
     <section>
         {{-- <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-
               <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div> --}}
             {{-- </div> --}}
             <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
@@ -253,12 +225,36 @@
 
         <div class="container-xxl">
 
-            <div class="img-fluid">
-                <img src="img/settings.png" class="img-fluid" alt="settings">
+            <div>
+                <p class="lh-lg">Food.com сайт который сделал для вас сайт, если вы владелец ресторана, кафе или кафе быстрого питание и вы хотите создать сайт для заказов онлайн то вы просто регистрируетесь называете как вы хотите назвать свой сайт, получаете домен 3го уровня food.example.com. Далее вам отправляется на почту логин и пароль на ваш сайт. Настройки сайта под себя для вас не займет большой проблемы. Все гайды по настройке вы можете посмотреть здесь.   </p>
             </div>
         </div>
     </section>
+    <footer class="container py-5">
+        <div class="row">
+          <div class="col-12 col-md">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+            <small class="d-block mb-3 text-muted">&copy; 2020-2020</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Features</h5>
+
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+
+          </div>
+          <div class="col-6 col-md">
+            <h5>About</h5>
+
+          </div>
+        </div>
+      </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
